a1626789748a2ee7f65ad2567734d7063f3b2e50
cafcaio <caio.abrantes@gmail.com>
2024-02-10 17:18:54 -0300

    Autoscaling groups

--- main.tf
+++ main.tf
@@ -36,20 +36,26 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  # insert the 1 required variable here
 
-subnet_id = module.blog_vpc.public_subnets[0]
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  ami                 = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
+
 }
 
-module "blog-alb" {
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
