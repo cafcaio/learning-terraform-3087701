cd7083a386d49189781b89fba67d1e161e6e9963
cafcaio <caio.abrantes@gmail.com>
2024-02-10 17:47:44 -0300

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 #data "aws_vpc" "default" {
@@ -24,15 +24,19 @@ data "aws_ami" "app_ami" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.env.name
+  cidr = "${var.env.network_prefix}.0.0/16"
 
   azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = [
+                      "${var.env.network_prefix}.101.0/24",
+                      "${var.env.network_prefix}.102.0/24",
+                      "${var.env.network_prefix}.103.0/24"
+                    ]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.env.name
   }
 }
 
@@ -42,15 +46,15 @@ module "autoscaling" {
   version = "7.3.1"
   # insert the 1 required variable here
 
-  name = "blog"
-  min_size = 1
-  max_size = 2
+  name = "${var.env.name}-blog"
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id                 = data.aws_ami.app_ami.id
+
+  image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
 
 }
@@ -62,14 +66,14 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  name    = "blog-alb"
+  name    = "${var.env.name}-blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.env.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -85,7 +89,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = var.env.name
   }
 }
 
@@ -94,7 +98,7 @@ module "blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name    = "blog_new"
+  name    = "${var.env.name}-blog"
 
   vpc_id               = module.blog_vpc.vpc_id
   ingress_rules        = ["http-80-tcp", "https-443-tcp"]
